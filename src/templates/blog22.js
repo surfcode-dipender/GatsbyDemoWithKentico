d95575d8c4ca39ed5e1824381dc070eb79e30663
dipendermohan86 <dipender@surfcode.io>
2020-01-30 16:21:07 +0530

    created the page

--- src/templates/blog22.js
+++ src/templates/blog22.js
@@ -1,24 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link, graphql } from 'gatsby';
 import Layout from '../components/layout';
 
 
 const BlogReference = ({data}) => {
-  
+  const item = data.allKontentItemBlogs.nodes[0].elements;
 
   return (
     <Layout>
       <div>
-      fafaafafs fsfsfwfqwrqwr
+       
+          <table>
+            <tbody>
+            <tr>
+              <th>Title:</th>
+              <td>{item.title.value}</td>
+            </tr>
+            <tr>
+              <th>URL:</th>
+              <td>{item.post_url.value}</td>
+            </tr>
+            <tr>
+              <th>Content:</th>
+              <td>
+               {item.content.value}
+               
+             </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </Layout>
   );
 };
 
+
+
+
 export default BlogReference;
-
-
-
+export const query = graphql`
+query speakingEngagementQuery($slug: String!){
+    allKontentItemBlogs(filter: {elements: {post_url: {value: {eq: $slug}}}}) {
+      nodes {
+        elements {
+          content {
+            value
+          }
+          post_url {
+            value
+          }
+          title {
+            value
+          }
+        }
+      }
+    }
+  }
+`;
 BlogReference.propTypes = {
   data: PropTypes.object,
 };
